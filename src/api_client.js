78886e126cbfd1002d69b4839accc8dd6a546c59
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2018-06-14 11:41:49 +0100

    Do not lint placeholder API client module

--- src/api_client.js
+++ src/api_client.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
 exports.send = function (message, recipient) {
-
+  // Placeholder function because we don't care about this part of the app for
+  // the purposes of the exercise
 };
