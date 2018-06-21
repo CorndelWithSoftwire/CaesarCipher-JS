59227922b7de82965326f32e461f2837aa395134
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2018-06-21 10:19:44 +0100

    Tweak the test which uses stubbed functions`

--- test/messageDispatcher.spec.js
+++ test/messageDispatcher.spec.js
@@ -35,15 +35,15 @@ describe('#send', function () {
     mockApiClient.verify();
   });
 
-  // Use stubbed crypt function
+  // Use stubbed functions
   it('sends an encrypted message', function () {
     const encrypt = this.sinon.stub(crypt, 'encrypt');
-    encrypt.withArgs('abcd', 8).returns('some encrypted message');
+    encrypt.withArgs('original message', 8).returns('some encrypted message');
 
     const mockApiClient = this.sinon.mock(apiClient);
     mockApiClient.expects('send').withArgs('some encrypted message', 'alice');
 
-    messageDispatcher.send('abcd', 'alice', 8);
+    messageDispatcher.send('original message', 'alice', 8);
 
     mockApiClient.verify();
   });
