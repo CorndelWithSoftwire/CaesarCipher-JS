2d9c4c05d3bb71cdcb966c423ed57a6edc2cefe2
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2018-07-09 10:43:32 +0100

    Add feature test for sending a message

--- features/app.feature
+++ features/app.feature
@@ -3,3 +3,7 @@ Feature: Send and receive messages
   Scenario: previewing a message
     When I preview the message "this is my deepest darkest secret" with shift 6
     Then I see the preview "znoy oy se jkkvkyz jgxqkyz ykixkz"
+
+  Scenario: sending a message
+    When I send the message "a secret message to send" to "Andre" with shift 15
+    Then the encrypted message "p htrgti bthhpvt id htcs" should be sent to "Andre"
--- features/support/steps.js
+++ features/support/steps.js
@@ -1,10 +1,33 @@
-const { Given, When, Then } = require('cucumber');
+const { Given, When, Then, BeforeAll } = require('cucumber');
 const { expect } = require('chai');
+const nock = require('nock');
+
+BeforeAll(function () {
+  nock.disableNetConnect();
+});
 
 When('I preview the message {string} with shift {int}', async function (message, shift) {
   this.previewedMessage = await this.controller.sendCommand(`preview message ${shift} ${message}`);
 });
 
+When('I send the message {string} to {string} with shift {int}', async function (message, recipient, shift) {
+  const context = this;
+
+  this.messageApi = nock('https://jsonplaceholder.typicode.com')
+    .post('/posts', function (body) {
+      context.messageBody = body;
+      return true;
+    })
+    .reply(200, {});
+
+  await this.controller.sendCommand(`send message ${recipient} ${shift} ${message}`);
+});
+
 Then('I see the preview {string}', function (encryptedMessage) {
   expect(this.previewedMessage).to.eq(encryptedMessage);
+});
+
+Then('the encrypted message {string} should be sent to {string}', function (encryptedMessage, recipient) {
+  expect(this.messageBody.userId).to.eq(recipient);
+  expect(this.messageBody.body).to.eq(encryptedMessage);
 });
